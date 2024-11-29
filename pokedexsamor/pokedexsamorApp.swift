a293b616f2623af1f9e5f169a8ecbd68e4a2d4c4
Samuel <Samor140@hotmail.com>
2024-11-29 08:58:48 -0500

    Initial Commit

---
+++ pokedexsamor/pokedexsamorApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexsamorApp.swift
+//  pokedexsamor
+//
+//  Created by Samuel Arseneault on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct pokedexsamorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
